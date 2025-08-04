e93704c3e117443530003516dc2f2bbc66790c23
Circulate233 <a18966009058@163.com>
2025-08-04 21:07:07 +0800

    修正复制机JEI的一些错误

--- src/main/java/github/kasuminova/novaeng/client/util/ExJEI.java
+++ src/main/java/github/kasuminova/novaeng/client/util/ExJEI.java
@@ -1,5 +1,6 @@
 package github.kasuminova.novaeng.client.util;
 
+import com.github.bsideup.jabel.Desugar;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import github.kasuminova.novaeng.NovaEngineeringCore;
@@ -12,13 +13,13 @@ import ink.ikx.rt.api.mods.jei.IJeiUtils;
 import ink.ikx.rt.impl.mods.jei.impl.core.MCJeiPanel;
 import ink.ikx.rt.impl.mods.jei.impl.core.MCJeiRecipe;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @SideOnly(Side.CLIENT)
 public class ExJEI{
@@ -51,12 +52,44 @@ public class ExJEI{
     }
 
     public static void jeiRecipeRegister() {
+        Map<SimpleItem, ItemStack> uniqueKeys = new HashMap<>();
+
         UuGraph.iterator().forEachRemaining(item -> {
             ItemStack stack = item.getKey().copy();
-            if (item.getValue() != Double.POSITIVE_INFINITY && !blockList.contains(Objects.requireNonNull(stack.getItem().getRegistryName()).getNamespace())) {
-                double bValue = item.getValue() / 100000;
-                new MCJeiRecipe("replicator_jei").addInput(CraftTweakerMC.getIItemStack(stack)).addOutput(CraftTweakerMC.getIItemStack(stack)).addElement(IJeiUtils.createFontInfoElement(I18n.format("gui." + NovaEngineeringCore.MOD_ID + ".replicator.tooltips1",Util.toSiString(bValue, 2)), 0, 20, 0x000000, 0, 0)).build();
+
+            ItemStack canonicalKey = uniqueKeys.computeIfAbsent(SimpleItem.getInstance(stack), k -> stack);
+
+            if (stack == canonicalKey) {
+                if (item.getValue() != Double.POSITIVE_INFINITY && !blockList.contains(Objects.requireNonNull(stack.getItem().getRegistryName()).getNamespace())) {
+                    double bValue = item.getValue() / 100000;
+                    new MCJeiRecipe("replicator_jei").addInput(CraftTweakerMC.getIItemStack(stack)).addOutput(CraftTweakerMC.getIItemStack(stack)).addElement(IJeiUtils.createFontInfoElement(I18n.format("gui." + NovaEngineeringCore.MOD_ID + ".replicator.tooltips1", Util.toSiString(bValue, 2)), 0, 20, 0x000000, 0, 0)).build();
+                }
             }
         });
+
+        uniqueKeys.clear();
+    }
+
+    @Desugar
+    private record SimpleItem(Item item, int meta, NBTTagCompound nbt) {
+
+        public static SimpleItem getInstance(ItemStack stack) {
+            return new SimpleItem(stack.getItem(), stack.getMetadata(), stack.getTagCompound());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            SimpleItem that = (SimpleItem) o;
+            return meta == that.meta && Objects.equals(item, that.item) && Objects.equals(nbt, that.nbt);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = item.hashCode();
+            result = 31 * result + meta;
+            result = 31 * result + (nbt != null ? nbt.hashCode() : 0);
+            return result;
+        }
     }
 }
