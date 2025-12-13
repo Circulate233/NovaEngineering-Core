86eb7d2cda3143b8896e47d00ab16dbb81bf9127
Circulate233 <a18966009058@163.com>
2025-12-13 23:23:59 +0800

    修复一个渲染问题

--- src/main/java/github/kasuminova/novaeng/mixin/minecraft/MixinGuiScreen.java
+++ src/main/java/github/kasuminova/novaeng/mixin/minecraft/MixinGuiScreen.java
@@ -3,6 +3,7 @@ package github.kasuminova.novaeng.mixin.minecraft;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +16,7 @@ public class MixinGuiScreen {
 
     @Inject(method = "handleComponentHover", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;renderToolTip(Lnet/minecraft/item/ItemStack;II)V"))
     protected void handleComponentHover(ITextComponent component, int x, int y, CallbackInfo ci, @Local(ordinal = 0) ItemStack itemstack) {
+        GlStateManager.color(1, 1, 1, 1);
         Minecraft.getMinecraft().getRenderItem().renderItemIntoGUI(itemstack, x - 18, y);
     }
 }
