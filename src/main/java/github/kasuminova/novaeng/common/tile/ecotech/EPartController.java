001d81a0c888e7a5bfe7351ca0c316cca4f26c73
Circulate233 <a18966009058@163.com>
2026-05-13 08:35:58 +0800

    对ECO设备补充一个在区块加载时要求结构重新检查

--- src/main/java/github/kasuminova/novaeng/common/tile/ecotech/EPartController.java
+++ src/main/java/github/kasuminova/novaeng/common/tile/ecotech/EPartController.java
@@ -141,7 +141,7 @@ public abstract class EPartController<P extends EPart<?>> extends TileCustomCont
         if (getControllerBlock().isInstance(state.getBlock())) {
             controllerRotation = state.getValue(FacingProp.HORIZONTALS);
         } else {
-            NovaEngineeringCore.log.warn("Invalid EPartController block at " + getPos() + " !");
+            NovaEngineeringCore.log.warn("Invalid EPartController block at {} !", getPos());
             controllerRotation = EnumFacing.NORTH;
         }
     }
@@ -168,6 +168,7 @@ public abstract class EPartController<P extends EPart<?>> extends TileCustomCont
 
     @Override
     public void onLoad() {
+        lastStructureCheckTick = -1;
         loaded = true;
     }
 
