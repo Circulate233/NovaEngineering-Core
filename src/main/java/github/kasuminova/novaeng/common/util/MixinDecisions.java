42c077da351385429628ccb035f45f8c59678c56
Circulate233 <a18966009058@163.com>
2026-09-14 23:33:26 +0800

    更新mixin

---
+++ src/main/java/github/kasuminova/novaeng/common/util/MixinDecisions.java
@@ -0,0 +1,102 @@
+package github.kasuminova.novaeng.common.util;
+
+import com.cleanroommc.discovery.CleanroomModDiscoverer;
+import github.kasuminova.novaeng.NovaEngCoreConfig;
+import net.minecraft.launchwrapper.Launch;
+
+import java.io.IOException;
+
+public final class MixinDecisions {
+
+    public static final boolean ae2Loaded = isPresent("appliedenergistics2");
+    public static final boolean alfheimLoaded = isPresent("alfheim");
+    public static final boolean arLoaded = isPresent("advancedrocketry");
+    public static final boolean actuallyAdditionsLoaded = isPresent("actuallyadditions");
+    public static final boolean astralSorceryLoaded = isPresent("astralsorcery");
+    public static final boolean athenaeumLoaded = isPresent("athenaeum");
+    public static final boolean betterP2pLoaded = isPresent("betterp2p");
+    public static final boolean botaniaLoaded = isPresent("botania");
+    public static final boolean codeChickenLibLoaded = isPresent("codechickenlib");
+    public static final boolean cofhLoaded = isPresent("cofhcore");
+    public static final boolean deepMobLearningLoaded = isPresent("deepmoblearning");
+    public static final boolean draconicEvolutionLoaded = isPresent("draconicevolution");
+    public static final boolean electroblobsLoaded = isPresent("ebwizardry");
+    public static final boolean enderioLoaded = isPresent("enderio");
+    public static final boolean extrabotanyLoaded = isPresent("extrabotany");
+    public static final boolean ic2Loaded = isPresent("ic2");
+    public static final boolean immersiveEngineeringLoaded = isPresent("immersiveengineering");
+    public static final boolean jeiLoaded = isPresent("jei");
+    public static final boolean jetifLoaded = isPresent("jetif");
+    public static final boolean legendaryTooltipsLoaded = isPresent("legendarytooltips");
+    public static final boolean libvulpesLoaded = isPresent("libvulpes");
+    public static final boolean lootOverhaulLoaded = isPresent("lootoverhaul");
+    public static final boolean metsLoaded = isPresent("mets");
+    public static final boolean modularRoutersLoaded = isPresent("modularrouters");
+    public static final boolean nae2Loaded = isPresent("nae2");
+    public static final boolean nuclearcraftLoaded = isPresent("nuclearcraft");
+    public static final boolean opticCheckLoaded = isPresent("opticheck");
+    public static final boolean packagedAutoLoaded = isPresent("packagedauto");
+    public static final boolean psiLoaded = isPresent("psi");
+    public static final boolean rftoolsLoaded = isPresent("rftools");
+    public static final boolean techgunsLoaded = isPresent("techguns");
+    public static final boolean threngLoaded = isPresent("threng");
+
+    private MixinDecisions() {
+    }
+
+    public static boolean shouldApply(final String mixinName) {
+        final int split = mixinName.indexOf('.');
+        if (split < 0) {
+            return true;
+        }
+
+        return switch (mixinName.substring(0, split)) {
+            case "dme" -> deepMobLearningLoaded && hasClassBytes("mustapelto.deepmoblearning.common.metadata.MetadataManager");
+            case "botania_r" -> botaniaLoaded && NovaEngCoreConfig.SERVER.bot;
+            case "ae2" -> ae2Loaded;
+            case "alfheim" -> alfheimLoaded;
+            case "ar" -> arLoaded;
+            case "actuallyadditions" -> actuallyAdditionsLoaded;
+            case "astralsorcery" -> astralSorceryLoaded;
+            case "athenaeum" -> athenaeumLoaded;
+            case "betterp2p" -> betterP2pLoaded;
+            case "botania" -> botaniaLoaded;
+            case "codechickenlib" -> codeChickenLibLoaded;
+            case "cofh" -> cofhLoaded;
+            case "draconicevolution" -> draconicEvolutionLoaded;
+            case "electroblobs" -> electroblobsLoaded;
+            case "enderio" -> enderioLoaded;
+            case "extrabotany" -> extrabotanyLoaded;
+            case "ic2" -> ic2Loaded;
+            case "immersiveengineering" -> immersiveEngineeringLoaded;
+            case "jei" -> jeiLoaded;
+            case "jetif" -> jetifLoaded;
+            case "legendarytooltips" -> legendaryTooltipsLoaded;
+            case "libvulpes" -> libvulpesLoaded;
+            case "lootoverhaul" -> lootOverhaulLoaded;
+            case "mets" -> metsLoaded;
+            case "modularrouters" -> modularRoutersLoaded;
+            case "nae2" -> nae2Loaded;
+            case "nco" -> nuclearcraftLoaded;
+            case "opticheck" -> opticCheckLoaded;
+            case "packagedauto" -> packagedAutoLoaded;
+            case "psi" -> psiLoaded;
+            case "rftools" -> rftoolsLoaded;
+            case "techguns" -> techgunsLoaded;
+            case "threng" -> threngLoaded;
+            default -> true;
+        };
+    }
+
+    private static boolean isPresent(final String modId) {
+        return CleanroomModDiscoverer.instance().isModPresent(modId);
+    }
+
+    private static boolean hasClassBytes(final String className) {
+        try {
+            return Launch.classLoader.getClassBytes(className) != null;
+        } catch (final IOException e) {
+            return false;
+        }
+    }
+}
--- src/main/java/github/kasuminova/novaeng/mixin/NovaEngCoreLateMixinLoader.java
+++ src/main/java/github/kasuminova/novaeng/mixin/NovaEngCoreLateMixinLoader.java
@@ -1,14 +1,15 @@
 package github.kasuminova.novaeng.mixin;
 
-import github.kasuminova.novaeng.NovaEngCoreConfig;
-import net.minecraftforge.fml.common.Loader;
+import github.kasuminova.novaeng.common.util.MixinDecisions;
+import github.kasuminova.novaeng.novaeng_core.Tags;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.util.List;
 import java.util.Set;
-import static net.minecraftforge.fml.common.Loader.isModLoaded;
 
 @SuppressWarnings({"unused", "SameParameterValue"})
 public class NovaEngCoreLateMixinLoader implements IMixinConfigPlugin {
@@ -17,6 +18,7 @@ public class NovaEngCoreLateMixinLoader implements IMixinConfigPlugin {
 
     @Override
     public void onLoad(final String mixinPackage) {
+        ConfigManager.sync(Tags.MOD_ID, Config.Type.INSTANCE);
     }
 
     @Override
@@ -31,50 +33,7 @@ public class NovaEngCoreLateMixinLoader implements IMixinConfigPlugin {
             mixinName = mixinName.substring(MIXIN_ROOT.length());
         }
 
-        int split = mixinName.indexOf('.');
-        if (split < 0) {
-            return true;
-        }
-
-        String group = mixinName.substring(0, split);
-
-        return switch (group) {
-            case "dme" -> isModLoaded("deepmoblearning")
-                && Loader.instance().getIndexedModList().get("deepmoblearning").getName().equals("DeepMobEvolution");
-            case "botania_r" -> isModLoaded("botania") && NovaEngCoreConfig.SERVER.bot;
-            case "ae2" -> isModLoaded("appliedenergistics2");
-            case "alfheim" -> isModLoaded("alfheim");
-            case "ar" -> isModLoaded("advancedrocketry");
-            case "actuallyadditions" -> isModLoaded("actuallyadditions");
-            case "astralsorcery" -> isModLoaded("astralsorcery");
-            case "athenaeum" -> isModLoaded("athenaeum");
-            case "betterp2p" -> isModLoaded("betterp2p");
-            case "botania" -> isModLoaded("botania");
-            case "codechickenlib" -> isModLoaded("codechickenlib");
-            case "cofh" -> isModLoaded("cofhcore");
-            case "draconicevolution" -> isModLoaded("draconicevolution");
-            case "electroblobs" -> isModLoaded("ebwizardry");
-            case "enderio" -> isModLoaded("enderio");
-            case "extrabotany" -> isModLoaded("extrabotany");
-            case "ic2" -> isModLoaded("ic2");
-            case "immersiveengineering" -> isModLoaded("immersiveengineering");
-            case "jei" -> isModLoaded("jei");
-            case "jetif" -> isModLoaded("jetif");
-            case "legendarytooltips" -> isModLoaded("legendarytooltips");
-            case "libvulpes" -> isModLoaded("libvulpes");
-            case "lootoverhaul" -> isModLoaded("lootoverhaul");
-            case "mets" -> isModLoaded("mets");
-            case "modularrouters" -> isModLoaded("modularrouters");
-            case "nae2" -> isModLoaded("nae2");
-            case "nco" -> isModLoaded("nuclearcraft");
-            case "opticheck" -> isModLoaded("opticheck");
-            case "packagedauto" -> isModLoaded("packagedauto");
-            case "psi" -> isModLoaded("psi");
-            case "rftools" -> isModLoaded("rftools");
-            case "techguns" -> isModLoaded("techguns");
-            case "threng" -> isModLoaded("threng");
-            default -> true;
-        };
+        return MixinDecisions.shouldApply(mixinName);
     }
 
     @Override
